23afa0e97a42d0e6464c1e56201566279ab2fe0c
Safwan Ahmad <innoxentsayyed@gmail.com>
2025-02-25 00:37:10 +0530

    creat app and add express,gitignore file and .env file

--- Devpoy/App.js
+++ Devpoy/App.js
@@ -1,1 +1,22 @@
-console.log("hello");
+require('dotenv').config();
+
+const express = require('express')
+const app = express()
+
+const port = 7000;
+app.get('/', (req, res) => {
+    res.send('<h1>Hello dear i am home page</h1>')
+})
+app.get('/service', (req, res) => {
+    res.send("<h1>hi i am service page</h1>")
+})
+app.get('/login', (req, res) => {
+    res.send('<h1>Please logIn this page</h1>')
+})
+app.get('/linkedin', (req, res) => {
+    res.send('<h1>welcome to linked in page</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port: http://localhost:${port}`)
+})
